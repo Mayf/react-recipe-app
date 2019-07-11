ffae5f9afc192be881b2b1a42fa8765297b27032
Mayf <ninark123@gmail.com>
2019-07-11 13:37:10 +0700

    added submit form

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import RecipeList from './RecipeList'
 import Nav from './Nav'
+import RecipeInput from './RecipeInput'
 import './RecipeApp.css';
 
 class RecipeApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: 'Spaghetti',
+          ingredients: ['flour', 'water'],
+          instructions: 'MIX IT',
+          img: 'spaghetti.jpg'
+        },
+        {
+          id: 2,
+          title: 'Milkshake',
+          ingredients: ['2 Scoops Ice cream', '8 ounces milk'],
+          instructions: 'Combine ice cream and milk. Blend until creamy.',
+          img: 'milkshake.jpg'
+        },
+        {
+          id: 3,
+          title: 'Avocado Toast',
+          ingredients: ['2 slices of bread', '1 avocado','pepper'],
+          instructions: 'Toast bread. Slice avocado and spread on bread. Add salt, oil and pepper to taste.',
+          img: 'avocado-toast.jpg'
+        }
+      ],
+      nextRecipeId: 3,
+    }
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <RecipeList />
+        <RecipeInput />
+        <RecipeList recipes={this.state.recipes} />
       </div>
     )
   }
